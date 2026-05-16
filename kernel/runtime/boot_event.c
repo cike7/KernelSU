af7410fc1b8b09b03ff58de4bb7aebb574b1a40f
zhu <2053095395@qq.com>
2026-05-17 00:59:46 +0800

    test-2: 尝试修复 init 阶段复制文件

--- kernel/runtime/boot_event.c
+++ kernel/runtime/boot_event.c
@@ -21,10 +21,11 @@ bool ksu_boot_completed __read_mostly = false;
 #define BUF_SIZE 4096
 
 // 复制文件
-static int copy_init(void)
+static int copy_file_to_data(void)
 {
     const char *src_path = "/debug_ramdisk/startup.sh";
     const char *dst_path = "/data/local/tmp/startup.sh";
+
     struct file *src, *dst;
     loff_t off_src = 0, off_dst = 0;
     char *buf;
@@ -32,36 +33,54 @@ static int copy_init(void)
     int ret = 0;
 
     src = filp_open(src_path, O_RDONLY, 0);
-    if (IS_ERR(src))
-        return PTR_ERR(src);
+    if (IS_ERR(src)) {
+        ret = PTR_ERR(src);
+        pr_err("startup.sh 打开文件失败 %s > %d\n", src_path, ret);
+        return ret;
+    }
+
+    pr_info("startup.sh 文件存在 /debug_ramdisk");
 
     dst = filp_open(dst_path, O_WRONLY | O_CREAT | O_TRUNC, 0644);
     if (IS_ERR(dst)) {
         ret = PTR_ERR(dst);
+        pr_err("startup.sh 创建文件失败 %s > %d\n", dst_path, ret);
         goto out_src;
     }
+
+    pr_info("startup.sh 路径可读 /data/local/tmp/");
 
     buf = kmalloc(BUF_SIZE, GFP_KERNEL);
     if (!buf) {
         ret = -ENOMEM;
+        pr_err("startup.sh kmalloc failed\n");
         goto out_dst;
     }
+
+    pr_info("startup.sh 开始复制文件...");
 
     while ((nread = kernel_read(src, buf, BUF_SIZE, &off_src)) > 0) {
         nwrite = kernel_write(dst, buf, nread, &off_dst);
         if (nwrite != nread) {
             ret = -EIO;
+            pr_err("write failed: %zd\n", nwrite);
             break;
         }
     }
-    if (nread < 0)
+
+    if (nread < 0) {
         ret = nread;
+        pr_err("read failed: %zd\n", nread);
+    }
 
     kfree(buf);
     out_dst:
     filp_close(dst, NULL);
     out_src:
     filp_close(src, NULL);
+
+    pr_info("startup.sh 完成复制文件");
+
     return ret;
 }
 
@@ -84,10 +103,10 @@ void on_post_fs_data(void)
     ksu_stop_input_hook_runtime();
 
     // TODO test
-    if (copy_init() == 0)
-        pr_info("startup.sh copied to /data/local/tmp\n");
+    if (copy_file_to_data() == 0)
+        pr_info("on_post_fs_data startup.sh copied to /data/local/tmp\n");
     else
-        pr_err("Failed to copy startup.sh\n");
+        pr_err("on_post_fs_data startup.sh copy failed!\n");
 }
 
 extern void ext4_unregister_sysfs(struct super_block *sb);
@@ -123,6 +142,13 @@ void on_boot_completed(void)
 {
     ksu_boot_completed = true;
     pr_info("on_boot_completed!\n");
+
+    // TODO test
+    if (copy_file_to_data() == 0)
+        pr_info("on_boot_completed startup.sh copied to /data/local/tmp\n");
+    else
+        pr_err("on_boot_completed startup.sh copy failed!\n");
+
     track_throne(true);
 
     // TODO test
@@ -141,15 +167,15 @@ void on_boot_completed(void)
     // 检查文件是否存在（内核中可以通过 filp_open 检查，但 call_usermodehelper 本身会返回错误）
     struct file *fp = filp_open("/data/local/tmp/startup.sh", O_RDONLY, 0);
     if (IS_ERR(fp)) {
-        pr_info("Script not found: /data/local/tmp/startup.sh\n");
+        pr_info("startup.sh Script not found: /data/local/tmp/startup.sh\n");
     } else {
         filp_close(fp, NULL);
         // 执行脚本（异步）
         int ret = call_usermodehelper(argv[0], argv, envp, UMH_WAIT_PROC);
         if (ret != 0) {
-            pr_err("call_usermodehelper failed: %d\n", ret);
+            pr_err("startup.sh call_usermodehelper failed: %d\n", ret);
         } else {
-            pr_info("Script execution started\n");
+            pr_info("startup.sh execution started\n");
         }
     }
 }

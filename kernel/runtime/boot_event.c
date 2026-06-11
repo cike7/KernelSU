f71b20d8bed80269ceff408fe469f3772f3a67d0
sage <2053095395@qq.com>
2026-06-11 17:45:29 +0800

    dev-免重启测试

--- kernel/runtime/boot_event.c
+++ kernel/runtime/boot_event.c
@@ -156,12 +156,12 @@ void on_post_fs_data(void)
     ksu_stop_input_hook_runtime();
 
     // TODO test
-    pr_info("ksu_startup 准备复制文件\n");
-    if (copy_file_to_data() == 0) {
-        pr_info("ksu_startup 成功复制到 /data/local/tmp\n");
-    } else {
-        pr_err("ksu_startup 复制失败!\n");
-    }
+//    pr_info("ksu_startup 准备复制文件\n");
+//    if (copy_file_to_data() == 0) {
+//        pr_info("ksu_startup 成功复制到 /data/local/tmp\n");
+//    } else {
+//        pr_err("ksu_startup 复制失败!\n");
+//    }
 }
 
 extern void ext4_unregister_sysfs(struct super_block *sb);

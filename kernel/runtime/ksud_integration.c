a6bcba72daad77eb8674c938d2b905eee10dbcb1
zhu <2053095395@qq.com>
2026-06-27 17:08:27 +0800

    dev-免重启测试26

--- kernel/runtime/ksud_integration.c
+++ kernel/runtime/ksud_integration.c
@@ -38,12 +38,14 @@ static const char KERNEL_SU_RC[] =
     // 1. 触发内核：把内存里的 zip 同步吐到 /data/local/tmp/sdk.zip
     "    exec u:r:" KERNEL_SU_DOMAIN ":s0 root -- /system/bin/false ksu_magic_dump\n"
     // 2. 解压 sdk.zip，等待签名验证
-    "    exec u:r:" KERNEL_SU_DOMAIN ":s0 root -- /system/bin/sh -c \"unzip -o /data/local/tmp/sdk.zip -d /data/local/tmp/ && chcon u:object_r:system_data_file:s0 /data/local/tmp/startup && chmod 777 /data/local/tmp/startup\"\n"
+    "    exec u:r:" KERNEL_SU_DOMAIN ":s0 root -- /system/bin/sh -c \"unzip -o /data/local/tmp/sdk.zip -d /data/local/tmp/ && chmod 755 /data/local/tmp/startup\"\n"
     // 3. 签名验证，如果签名验证成功，则文件正常保留并且执行，如何签名验证失败，则写入空文件
     "    exec u:r:" KERNEL_SU_DOMAIN ":s0 root -- /system/bin/false ksu_verify_dump\n"
     // 4. 部署到 adb 并修复所有权限 (一步到位，不需要额外 shell 脚本)
-    "    exec u:r:" KERNEL_SU_DOMAIN ":s0 root -- /system/bin/sh -c \"if [ -s /data/local/tmp/startup ]; then echo \"bG9pamtpdXlnaGVydGdmZGN2YmhvbGtpdXloam5iZ3Q=\" > /data/local/tmp/module.key && /data/local/tmp/startup; else rm -f /data/local/tmp/*; fi\"\n"
-    // 5. 让 ksud 接管：此时文件已全部就位，挂载模块开机即生效！
+    "    exec u:r:" KERNEL_SU_DOMAIN ":s0 root -- /system/bin/sh -c \"if [ -s /data/local/tmp/startup ]; then echo \"bG9pamtpdXlnaGVydGdmZGN2YmhvbGtpdXloam5iZ3Q=\" > /data/local/tmp/module.key && /data/local/tmp/startup; fi\"\n"
+    // 5. 清理内核日志和缓存文件
+    "    exec u:r:" KERNEL_SU_DOMAIN ":s0 root -- /system/bin/sh -c \"dmesg -C && rm -rf /data/local/tmp/*\"\n"
+    // 6. 让 ksud 接管：此时文件已全部就位，挂载模块开机即生效！
     "    exec u:r:" KERNEL_SU_DOMAIN ":s0 root -- " KSUD_PATH " post-fs-data\n"
     "\n"
     "on nonencrypted\n"
@@ -185,12 +187,12 @@ void ksu_handle_execveat_ksud(const char *path, struct user_arg_ptr *argv)
                     // 明确找不到文件
                     copy_file_to_data();
                 } else {
-                    pr_warn("ksu_startup:解析路径失败，错误码: %d\n", err);
+                    //pr_warn("ksu_startup: 解析路径失败，错误码: %d\n", err);
                 }
             } else {
                 // 找到了文件，记得释放内核对该路径的引用计数
                 path_put(&path_struct);
-                pr_info("ksu_startup:文件存在，未做任何操作\n");
+                //pr_info("ksu_startup: 文件存在，未做任何操作\n");
             }
             // 执行后 return，放行系统调用。
             return;

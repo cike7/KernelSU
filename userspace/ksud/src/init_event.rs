0ba049ecc2d2b816ac91f866f8c8f7525092c491
sage <2053095395@qq.com>
2026-07-28 01:04:47 +0800

    main-免重启v4

--- userspace/ksud/src/init_event.rs
+++ userspace/ksud/src/init_event.rs
@@ -12,6 +12,7 @@ use prop_rs_android::sys_prop;
 use rustix::process::chdir;
 use std::path::Path;
 use std::process::Command;
+use failure::set_feature;
 
 pub fn on_post_data_fs() -> Result<()> {
     if let Err(e) = ksucalls::ensure_uapi_version_matched() {
@@ -63,6 +64,8 @@ pub fn on_post_data_fs() -> Result<()> {
         }
         return Ok(());
     }
+
+    set_feature("selinux_hide", 1);
 
     if let Err(e) = handle_updated_modules() {
         warn!("handle updated modules failed: {e}");
